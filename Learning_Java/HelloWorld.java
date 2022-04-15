8539ffcfc14463b39f013303ad5c396a4db00077
Luis Escobar <lae311@stern.nyu.edu>
2022-04-15 17:09:50 -0400

    first java code

---
+++ Learning_Java/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld{
+public static void main(String[] args) {
+    System.out.println("Hello World");
+    }
+}
+// A file has to be named the same as the parent class
+// Unlike js, strings have to be in double quotes (at least so far)
